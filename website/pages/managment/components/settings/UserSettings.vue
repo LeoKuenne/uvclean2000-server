740cc691a9e3990b8d13ef2238bfa728cdda7eea
Leo <leo@k-ku3nn3.de>
2021-03-29 12:08:19 +0200

    Fixed overflow scrolling on user settings view

--- website/pages/managment/components/settings/UserSettings.vue
+++ website/pages/managment/components/settings/UserSettings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col xl:flex-row h-full w-full overflow-scroll">
+  <div class="flex flex-col xl:flex-row h-full w-full overflow-y-auto">
     <user-list class="w-full xl:w-1/2"></user-list>
     <userrole-list class="w-full xl:w-1/2"></userrole-list>
   </div>
